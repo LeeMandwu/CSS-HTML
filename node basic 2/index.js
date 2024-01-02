25a13ebd64bc1e1e1eddec7575add08e6b31138e
LeeMandwu <109013487+LeeMandwu@users.noreply.github.com>
2024-01-02 22:59:25 +0900

    DB없이 댓글창 만들기❤️‍🔥
    ejs, get, post, db

--- node basic 2/index.js
+++ node basic 2/index.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var app = express();
 
+let comments = [];
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 // index page
 app.get('/', function(req, res) {
-  res.render('index',{ num : 3});
+  res.render('index',{ comments: comments });
+});
+
+app.post('/create', function(req, res) {
+  console.log(req.body)
+  
+  const { content } = req.body
+
+  comments.push(content)
+  
+  console.log(comments)
+  
+  res.redirect('/')
 });
 
 app.listen(3000);
