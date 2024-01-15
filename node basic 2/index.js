68d64df2a6b8331268d2ee4000a5c57382a457ee
LeeMandwu <109013487+LeeMandwu@users.noreply.github.com>
2024-01-15 22:46:05 +0900

    CRUD게시판🪄

--- node basic 2/index.js
+++ node basic 2/index.js
@@ -1,30 +1,81 @@
 var express = require('express');
 var app = express();
 
-let comments = [];
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+});
+
+const Comments = sequelize.define('Comments', {
+  // Model attributes are defined here
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  // Other model options go here
+});
+
+(async() => {
+await Comments.sync();
+})();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // index page
-app.get('/', function(req, res) {
-  res.render('index',{ comments: comments });
+app.get('/', async function(req, res) {
+  const comments = await Comments.findAll();
+
+  res.render('index',{ comments : comments }); 
 });
 
-app.post('/create', function(req, res) {
+app.post('/create', async function(req, res) {
   console.log(req.body)
   
   const { content } = req.body
 
-  comments.push(content)
+  // Create a new user
+  await Comments.create({content : content});
+ 
+  res.redirect('/')
+});
+
+app.post('/update/:id', async function(req, res) {
+  console.log(req.params)
+  console.log(req.body)
+
   
-  console.log(comments)
+  const { content } = req.body
+  const { id } = req.params
+
+  // Change everyone without a last name to "Doe"
+  await Comments.update({ content : content }, {
+    where: { 
+     id: id
+  }
+});
+
+res.redirect('/')
+});
+
+app.post('/delete/:id', async function(req, res) {
+  console.log(req.params)
+  console.log(req.body)
+
+  const { id } = req.params
+
+await Comments.destroy({
+  where: {
+    id:id
+  }
+});
   
   res.redirect('/')
 });
